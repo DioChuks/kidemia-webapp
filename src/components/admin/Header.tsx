4f543bf775c1f2b67d4164a2e664879d4487d611
DioChuks <diochuks65@gmail.com>
2024-11-01 17:46:09 +0100

    chore: removed bottombar and used existing mobile menu ui, added logout

--- src/components/admin/Header.tsx
+++ src/components/admin/Header.tsx
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import Logo from '../icons/Logo';
 import BellIcon from '../icons/BellIcon';
 import MenuIcon from '../icons/MenuIcon';
-import BottomBar from './BottomBar';
+import ReportLineIcon from '../icons/ReportLine';
+import BooksIcon from '../icons/Books';
+import { AuthContext } from '../../contexts/AuthContext';
 
 const Header: React.FC = () => {
+  const {logout} = useContext(AuthContext)
   const [isModalOpen, setIsModalOpen] = useState(false);
   const handleModalToggle = () => setIsModalOpen(!isModalOpen);
   return (
@@ -12,21 +15,26 @@ const Header: React.FC = () => {
     <Logo />
     {/* {{-- medium to large screen --}} */}
         <nav className="flex items-center nav sm-d-none">
-          <ul className="flex items-center">
+          <ul className="flex items-center relative">
             <li className="nav-item"><a href="#"><BellIcon/></a></li>
-            <li className="text-white nav-item">Admin</li>
+            <li className="text-white nav-item cursor-pointer" onClick={handleModalToggle}>Admin</li>
+            {isModalOpen && (<ul
+            className="absolute top-3 right-2 w-10 h-4 border flex flex-col justify-around items-center bg-brand-white rounded-sm">
+            <li className="text-red nav-item font-lg cursor-pointer" onClick={logout}>Logout</li>
+          </ul>)}
         </ul>
         </nav>
         {/* {{-- small screen --}} */}
-        <nav className="relative hidden md-d-none z-10" id="mobile-menu">
+        {isModalOpen && (<nav className="relative md-d-none z-10" id="mobile-menu">
           <ul
             className="absolute top-2 right-2 left-2 w-20 h-30 border flex flex-col justify-around items-center bg-brand-white rounded-sm">
+            <a href="/admin/dashboard/report" className="flex items-center gap-1 text-dark"><ReportLineIcon /> Report</a>
+            <a href="/admin/dashboard/subjects" className="flex items-center gap-1 text-dark"><BooksIcon /> Subjects</a>
             <a href="#" className="flex items-center gap-1 text-dark"><BellIcon /> Notifications</a>
-            <li className="text-white nav-item">Admin</li>
+            <li className="text-red nav-item font-lg" onClick={logout}>Logout</li>
           </ul>
-        </nav>
-        <span onClick={handleModalToggle}><MenuIcon/></span>
-        {isModalOpen && <BottomBar/>}
+        </nav>)}
+        <span className='md-d-none' onClick={handleModalToggle}><MenuIcon/></span>
   </header>
   )
 };
