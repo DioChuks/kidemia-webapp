eeb0effe3c5aeb38ea0b1ec1657c857adb54c315
DioChuks <diochuks65@gmail.com>
2024-11-12 12:05:52 +0100

    fix: unauthorized

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const authUser = sessionStorage.getItem('userData');
-const token = authUser ? JSON.parse(authUser): undefined;
+const parsed = authUser ? JSON.parse(authUser): undefined;
 
 const api = axios.create({
   baseURL: 'https://kidemia.silfrica.com/api',
   timeout: 10000,
   headers: {
     Accept: 'application/json',
-    Authorization: token ? `Bearer ${token}` : undefined,
+    Authorization: parsed.token ? `Bearer ${parsed.token}` : undefined,
   },
 });
 
