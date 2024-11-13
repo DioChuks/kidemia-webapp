0f95e9654da8d98c86a51fa388957222fed7be39
DioChuks <diochuks65@gmail.com>
2024-11-13 20:33:03 +0100

    fix: Uncaught TypeError: Cannot read properties of undefined (reading 'token')

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,14 +1,19 @@
 import axios from 'axios';
 
+interface Session {
+  user: object;
+  token: string;
+}
+
 const authUser = sessionStorage.getItem('userData');
-const parsed = authUser ? JSON.parse(authUser): undefined;
+const parsed: Session = authUser ? JSON.parse(authUser): undefined;
 
 const api = axios.create({
   baseURL: 'https://kidemia.silfrica.com/api',
   timeout: 10000,
   headers: {
     Accept: 'application/json',
-    Authorization: parsed.token ? `Bearer ${parsed.token}` : undefined,
+    Authorization: parsed && parsed.token ? `Bearer ${parsed.token}` : undefined,
   },
 });
 
