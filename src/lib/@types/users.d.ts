ade45eb87c12286f72ebf49e1deb8646869d1074
DioChuks <diochuks65@gmail.com>
2024-11-28 20:22:45 +0100

    feat: user states interface

---
+++ src/lib/@types/users.d.ts
@@ -0,0 +1,26 @@
+export interface IAuthUser {
+    user: {
+        id: number;
+        category_id: number;
+        name: string;
+        email: string;
+        email_verified_at: null | Date;
+        photo: null | string;
+        otp: null | string;
+        category_status: boolean;
+        guardian_email: null | string;
+        role: string;
+        created_at: Date;
+        updated_at: Date;
+    }
+    token: string;
+}
+
+export interface IRegisterUser {
+    type: "student" | "school";
+    email: string;
+    password: string;
+    confirm_password: string;
+    purpose: number | null;
+    guardian_email: string;
+}
