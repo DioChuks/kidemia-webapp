9867f3686d79c357ec5b054695078bb15c1574eb
DioChuks <diochuks65@gmail.com>
2024-11-28 20:17:53 +0100

    chore: loading component updated styles

--- src/components/modals/Loading.tsx
+++ src/components/modals/Loading.tsx
@@ -7,7 +7,7 @@ const Loading: React.FC = () => {
       id="loadingMode"
       className="fixed z-10 w-max-screen h-max-screen flex items-center justify-center bg-semi-black"
     >
-      <div className="w-half h-half flex justify-center items-center bg-brand-white rounded-md z-99">
+      <div className="w-half h-half flex justify-center items-center rounded-md z-99">
         <div className="h-10 transition-all animate-show-border">
           <img src={Logo2} alt="loading..." className="animate-spin-off" />
         </div>
