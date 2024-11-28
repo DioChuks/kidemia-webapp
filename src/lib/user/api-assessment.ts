bf7927c72589e4428ad581b4c5fb07f903de19c0
DioChuks <diochuks65@gmail.com>
2024-11-28 20:24:20 +0100

    chore: refactored & cleaned api for assessment

--- src/lib/user/api-assessment.ts
+++ src/lib/user/api-assessment.ts
@@ -1,24 +1,12 @@
-import { AsTest } from "../@types/assessments";
-import { Topic } from "../@types/topics";
+import { AsExam, AsTest } from "../@types/assessments";
 import api from "../api";
 import ApiResponse from "../res";
 
-export const fetchTopics = async (): Promise<Topic[]> => {
-    const response = await api.get<ApiResponse<Topic[]>>('/topics');
-    return response.data.data;
-}
-
-export const fetchTopicsBySubjectId = async (id: string): Promise<Topic[]> => {
-    const response = await api.get<ApiResponse<Topic[]>>(`/topics/subjects/${id}`);
-    return response.data.data;
-}
-
-export const fetchTopic = async (id:string|number): Promise<Topic> => {
-    const response = await api.get<ApiResponse<Topic>>(`/topics/${id}`);
-    return response.data.data;
-}
-
-export const createTestOrExam = async (data: any): Promise<AsTest> => {
-    const response = await api.post<ApiResponse<AsTest>>('/assessments', data);
+export const createTestOrExam = async (data: any, type: string): Promise<AsExam | AsTest> => {
+    if (type === 'exam') {
+        const response = await api.post<ApiResponse<AsExam>>('/assessments/exams', data);
+        return response.data.data;
+    }
+    const response = await api.post<ApiResponse<AsTest>>('/assessments/tests', data);
     return response.data.data;
 }
