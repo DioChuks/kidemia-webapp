6bec69e025a9a020f98f2b5bbfa251a4633ca7ad
DioChuks <diochuks65@gmail.com>
2024-11-28 20:23:25 +0100

    chore: questions bulk import api

--- src/lib/admin/api-questions.ts
+++ src/lib/admin/api-questions.ts
@@ -2,6 +2,12 @@ import { NewQuestion, Question } from "../@types/questions";
 import api from "../api";
 import ApiResponse from "../res";
 
+const fileUploadConfig = {
+    headers: {
+        'Content-Type': 'multipart/form-data',
+    },
+}
+
 export const fetchQuestions = async (): Promise<Question[]> => {
     const response = await api.get<ApiResponse<Question[]>>('/questions');
     return response.data.data;
@@ -10,6 +16,11 @@ export const fetchQuestions = async (): Promise<Question[]> => {
 export const storeQuestion = async (data: FormData): Promise<NewQuestion> => {
     const response = await api.post<ApiResponse<NewQuestion>>('/questions', data);
     return response.data.data;
+}
+
+export const importQuestions = async (data: FormData) => {
+    const response = await api.post<ApiResponse<{}>>('/questions/import', data, fileUploadConfig);
+    return response.data.message;
 }
 
 export const fetchQuestion = async (id:string|number): Promise<Question> => {
