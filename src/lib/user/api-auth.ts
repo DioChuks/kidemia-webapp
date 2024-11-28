4be0f24723151ab1ad1c49dfebabd1e795450467
DioChuks <diochuks65@gmail.com>
2024-11-28 20:26:09 +0100

    chore: user api updated to have register call(), cleanup

--- src/lib/user/api-auth.ts
+++ src/lib/user/api-auth.ts
@@ -1,26 +1,8 @@
+import { IAuthUser, IRegisterUser } from "../@types/users";
 import { env } from "../api";
 
-// Define the interfaces for the expected data structures
-interface User {
-  user: {
-    id: number;
-    category_id?: number;
-    name: string;
-    email: string;
-    email_verified_at: null | Date;
-    photo: null | string;
-    otp: null | string;
-    category_status: boolean;
-    guardian_email: null | string;
-    role: string;
-    created_at: Date;
-    updated_at: Date;
-  }
-  token: string;
-}
-
 // Admin Login function
-export const attemptLogin = async (email: string, password: string): Promise<User> => {
+export const attemptLogin = async (email: string, password: string): Promise<IAuthUser> => {
   const response = await fetch(`${env.prod}/auth/login`, {
     method: "POST",
     body: JSON.stringify({ email: email, password: password }),
@@ -36,3 +18,20 @@ export const attemptLogin = async (email: string, password: string): Promise<Use
   
   return json.data
 };
+
+export const attemptRegister = async (data: IRegisterUser): Promise<IAuthUser> => {
+  const response = await fetch(`${env.prod}/auth/register`, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    }
+  });
+  const json = await response.json();
+  if (!response.ok) {
+    throw new Error(json.message)
+  }
+  
+  return json.data
+}
